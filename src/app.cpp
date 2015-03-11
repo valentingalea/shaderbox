391cbd898d7b0d4bc53e5142c16ec1e79acf3b31
Valentin Galea <valentingalea@gmail.com>
2015-03-11 23:38:35 +0000

    refactor to split the sdf egg

---
+++ src/app.cpp
@@ -0,0 +1,565 @@
+// adapted from Piotr Gwiazdowski <gwiazdorrr+github at gmail.com>
+
+#include <swizzle/glsl/naive/vector.h>
+#include <swizzle/glsl/naive/matrix.h>
+#include <swizzle/glsl/texture_functions.h>
+
+typedef swizzle::glsl::naive::vector< int, 2 > ivec2;
+typedef swizzle::glsl::naive::vector< float, 2 > vec2;
+typedef swizzle::glsl::naive::vector< float, 3 > vec3;
+typedef swizzle::glsl::naive::vector< float, 4 > vec4;
+
+static_assert(sizeof(vec2) == sizeof(float[2]), "Too big");
+static_assert(sizeof(vec3) == sizeof(float[3]), "Too big");
+static_assert(sizeof(vec4) == sizeof(float[4]), "Too big");
+
+typedef swizzle::glsl::naive::matrix< swizzle::glsl::naive::vector, float, 2, 2> mat2;
+typedef swizzle::glsl::naive::matrix< swizzle::glsl::naive::vector, float, 3, 3> mat3;
+typedef swizzle::glsl::naive::matrix< swizzle::glsl::naive::vector, float, 4, 4> mat4;
+
+//! A really, really simplistic sampler using SDLImage
+struct SDL_Surface;
+class sampler2D : public swizzle::glsl::texture_functions::tag
+{
+public:
+	enum WrapMode
+	{
+		Clamp,
+		Repeat,
+		MirrorRepeat
+	};
+
+	typedef vec2 tex_coord_type;
+
+	sampler2D(const char* path, WrapMode wrapMode);
+	~sampler2D();
+	vec4 sample(vec2 coord);
+
+private:
+	SDL_Surface *m_image;
+	WrapMode m_wrapMode;
+
+	// do not allow copies to be made
+	sampler2D(const sampler2D&);
+	sampler2D& operator=(const sampler2D&);
+};
+
+// this where the magic happens...
+namespace glsl_sandbox
+{
+#include <swizzle/glsl/vector_functions.h>
+
+	// constants shaders are using
+	float time;
+	vec2 mouse;
+	vec2 resolution;
+
+	// constants some shaders from shader toy are using
+	vec2& iResolution = resolution;
+	float& iGlobalTime = time;
+	vec2& iMouse = mouse;
+
+	sampler2D diffuse("diffuse.png", sampler2D::Repeat);
+	sampler2D specular("specular.png", sampler2D::Repeat);
+
+	struct fragment_shader
+	{
+		vec2 gl_FragCoord;
+		vec4 gl_FragColor;
+		void mainImage(vec4 &fragColor, const vec2 &fragCoord);
+	};
+
+	// change meaning of glsl keywords to match sandbox
+#define uniform extern
+#define mainImage fragment_shader::mainImage
+
+#pragma warning(push)
+#pragma warning(disable: 4244) // disable return implicit conversion warning
+#pragma warning(disable: 4305) // disable truncation warning
+
+#include "egg.h"
+	
+#pragma warning(pop)
+#undef main
+#undef uniform
+}
+
+#define OMP_ENABLED 1
+
+// these headers, especially SDL.h & time.h set up names that are in conflict
+// with sandbox'es;
+// sandbox should be moved to a separate h/cpp pair, but out of laziness...
+// including them
+// *after* sandbox solves it too
+
+#include <iostream>
+#include <sstream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <time.h>
+#include <memory>
+#include <functional>
+#ifdef OMP_ENABLED
+#include <omp.h>
+#endif
+
+//! A handy way of creating (and checking) unique_ptrs of SDL objects
+template <class T>
+std::unique_ptr< T, std::function<void(T*)> > makeUnique(T* value, std::function<void(T*)> deleter)
+{
+	if (!value)
+	{
+		throw std::runtime_error("Null pointer");
+	}
+	return std::unique_ptr<T, decltype(deleter)>(value, deleter);
+}
+
+//! As above, but allows null initialisation
+template <class T>
+std::unique_ptr< T, std::function<void(T*)> > makeUnique(std::function<void(T*)> deleter)
+{
+	return std::unique_ptr<T, decltype(deleter)>(nullptr, deleter);
+}
+
+//! Just a RAII wrapper around SDL_mutex
+struct ScopedLock
+{
+	SDL_mutex* mutex;
+
+	explicit ScopedLock(SDL_mutex* mutex) : mutex(mutex)
+	{
+		SDL_LockMutex(mutex);
+	}
+
+	template <class T>
+	explicit  ScopedLock(std::unique_ptr<SDL_mutex, T>& mutex) : mutex(mutex.get())
+	{
+		SDL_LockMutex(this->mutex);
+	}
+
+	~ScopedLock()
+	{
+		SDL_UnlockMutex(mutex);
+	}
+};
+
+
+//! The surface to draw on.
+auto g_surface = makeUnique<SDL_Surface>(SDL_FreeSurface);
+//! Mutex used when exchaning frame between threads
+auto g_frameHandshakeMutex = makeUnique<SDL_mutex>(SDL_CreateMutex(), SDL_DestroyMutex);
+//! Signaled when a frame has been processed
+auto m_frameReceivedEvent = makeUnique<SDL_cond>(SDL_CreateCond(), SDL_DestroyCond);
+//! Signaled when a frame is ready to be processed
+auto m_frameReadyEvent = makeUnique<SDL_cond>(SDL_CreateCond(), SDL_DestroyCond);
+//! Additional flag set when a frame becomes ready, in case main thread is not waiting
+bool g_frameReady = false;
+//! Stop drawing
+bool g_cancelDraw = false;
+//! Quit!
+bool g_quit = false;
+
+//#define WRITE_GIF
+#ifdef WRITE_GIF
+#include "..\lib\gif-h\gif.h"
+#include <time.h>
+#endif
+
+//! Thread used for rendering; it invokes the shader
+static int renderThread(void*)
+{
+	while (true)
+	{
+		auto bmp = g_surface.get();
+
+#if !defined(_DEBUG) && OMP_ENABLED
+#pragma omp parallel 
+		{
+			int thredsCount = omp_get_num_threads();
+			int threadNum = omp_get_thread_num();
+
+			int heightPerThread = bmp->h / thredsCount;
+			int heightStart = threadNum * heightPerThread;
+			int heightEnd = (threadNum == thredsCount - 1) ? bmp->h : (heightStart + heightPerThread);
+#else
+		{
+				int heightStart = 0;
+				int heightEnd = bmp->h;
+#endif
+
+				glsl_sandbox::fragment_shader shader;
+				for (int y = heightStart; !g_cancelDraw && y < heightEnd; ++y)
+				{
+					uint8_t * ptr = reinterpret_cast<uint8_t*>(bmp->pixels) + y * bmp->pitch;
+					for (int x = 0; x < bmp->w; ++x)
+					{
+						shader.gl_FragCoord = vec2(static_cast<float>(x), bmp->h - 1.0f - y);
+
+						// vvvvvvvvvvvvvvvvvvvvvvvvvv
+						// THE SHADER IS INVOKED HERE
+						// ^^^^^^^^^^^^^^^^^^^^^^^^^^
+						shader.mainImage(shader.gl_FragColor, shader.gl_FragCoord);
+
+						auto color = glsl_sandbox::clamp(shader.gl_FragColor, 0.0f, 1.0f);
+
+#ifdef WRITE_GIF
+						*ptr++ = 0;
+						*ptr++ = static_cast<uint8_t>(255 * color.r + 0.5f);
+						*ptr++ = static_cast<uint8_t>(255 * color.g + 0.5f);
+						*ptr++ = static_cast<uint8_t>(255 * color.b + 0.5f);
+#else
+						*ptr++ = static_cast<uint8_t>(255 * color.r + 0.5f);
+						*ptr++ = static_cast<uint8_t>(255 * color.g + 0.5f);
+						*ptr++ = static_cast<uint8_t>(255 * color.b + 0.5f);
+#endif
+					}
+				}
+			}
+
+		ScopedLock lock(g_frameHandshakeMutex);
+		if (g_quit)
+		{
+			return 0;
+		}
+		else
+		{
+			// frame is ready, change bool and raise signal (in case main thread is waiting)
+			g_frameReady = true;
+			SDL_CondSignal(m_frameReadyEvent.get());
+
+			// wait for the main thread to process the frame
+			SDL_CondWait(m_frameReceivedEvent.get(), g_frameHandshakeMutex.get());
+			if (g_quit)
+			{
+				return 0;
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	using namespace std;
+
+	// initialise SDLImage
+	int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+	int initted = IMG_Init(flags);
+	if ((initted & flags) != flags)
+	{
+		cerr << "WARNING: failed to initialise required jpg and png support: " << IMG_GetError() << endl;
+	}
+
+	// get initial resolution
+	ivec2 initialResolution(SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (argc == 2)
+	{
+		std::stringstream s;
+		s << argv[1];
+		if (!(s >> initialResolution))
+		{
+			cerr << "ERROR: unable to parse resolution argument" << endl;
+			return 1;
+		}
+	}
+
+	if (initialResolution.x <= 0 || initialResolution.y < 0)
+	{
+		cerr << "ERROR: invalid resolution: " << initialResolution << endl;
+		return 1;
+	}
+
+	cout << "\n";
+	cout << "+/-   - increase/decrease time scale\n";
+	cout << "lmb   - update glsl_sandbox::mouse\n";
+	cout << "space - blit now! (show incomplete render)\n";
+	cout << "esc   - quit\n\n";
+
+#ifdef WRITE_GIF
+	time_t rawtime;
+	time(&rawtime);
+	tm timeinfo;
+	localtime_s(&timeinfo, &rawtime);
+
+	char gif_file[128];
+	strftime(gif_file, 128, "anim-%Y-%m-%d-%H-%M-%S.gif\0", &timeinfo);
+
+	GifWriter gif;
+	if (!GifBegin(&gif, gif_file, initialResolution.x, initialResolution.y, 0)) return 1;
+#endif
+
+	// it doesn't need cleaning up
+	SDL_Surface* screen = nullptr;
+
+	try
+	{
+#ifdef WRITE_GIF
+		int bit_depth = 32;
+		int mask_r = 0xff000000;
+		int mask_g = 0x00ff0000;
+		int mask_b = 0x0000ff00;
+#else
+		int bit_depth = 24;
+		int mask_r = 0x000000ff;
+		int mask_g = 0x0000ff00;
+		int mask_b = 0x00ff0000;
+#endif
+
+		// a function to resize the screen; throws if unsuccessful
+		auto resizeOrCreateScreen = [&](int w, int h) -> void
+		{
+			screen = SDL_SetVideoMode(w, h, bit_depth, SDL_SWSURFACE | SDL_RESIZABLE);
+			if (!screen)
+			{
+				throw std::runtime_error("Unable to set video mode");
+			}
+		};
+
+		// a function used to resize the surface
+		auto resizeOrCreateSurface = [&](int w, int h) -> void
+		{
+			g_surface.reset(SDL_CreateRGBSurface(SDL_SWSURFACE, w, h, bit_depth, mask_r, mask_g, mask_b, 0));
+			if (!g_surface)
+			{
+				throw std::runtime_error("Unable to create surface");
+			}
+			// update shader value
+			glsl_sandbox::resolution.x = static_cast<float>(w);
+			glsl_sandbox::resolution.y = static_cast<float>(h);
+		};
+
+		// initial setup
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			throw std::runtime_error("Unable to init SDL");
+		}
+		SDL_EnableKeyRepeat(200, 16);
+		SDL_WM_SetCaption("SDL/Swizzle", "SDL/Swizzle");
+
+		resizeOrCreateScreen(initialResolution.x, initialResolution.y);
+		resizeOrCreateSurface(initialResolution.x, initialResolution.y);
+
+		float timeScale = 1;
+		int frame = 0;
+		float time = 0;
+		vec2 mousePosition;
+		bool pendingResize = false;
+		bool mousePressed = false;
+
+
+		auto renderThreadInstance = SDL_CreateThread(renderThread, nullptr);
+
+		clock_t begin = clock();
+
+		while (!g_quit)
+		{
+			bool blitNow = false;
+
+			// process events
+			SDL_Event event;
+			while (SDL_PollEvent(&event))
+			{
+				switch (event.type)
+				{
+				case SDL_VIDEORESIZE:
+					if (event.resize.w != screen->w || event.resize.h != screen->h)
+					{
+						resizeOrCreateScreen(event.resize.w, event.resize.h);
+						ScopedLock lock(g_frameHandshakeMutex);
+						g_cancelDraw = pendingResize = true;
+					}
+					break;
+				case SDL_QUIT:
+				{
+								 ScopedLock lock(g_frameHandshakeMutex);
+								 g_quit = g_cancelDraw = true;
+				}
+					break;
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.sym)
+					{
+					case SDLK_SPACE:
+						blitNow = true;
+						break;
+					case SDLK_ESCAPE:
+					{
+										ScopedLock lock(g_frameHandshakeMutex);
+										g_quit = g_cancelDraw = true;
+					}
+						break;
+					case SDLK_PLUS:
+					case SDLK_EQUALS:
+						timeScale *= 2.0f;
+						break;
+					case SDLK_MINUS:
+						timeScale /= 2.0f;
+						break;
+					default:
+						break;
+					}
+					break;
+				case SDL_MOUSEMOTION:
+					if (mousePressed)
+					{
+						mousePosition.x = static_cast<float>(event.button.x);
+						mousePosition.y = static_cast<float>(g_surface->h - 1 - event.button.y);
+					}
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					mousePressed = true;
+					mousePosition.x = static_cast<float>(event.button.x);
+					mousePosition.y = static_cast<float>(g_surface->h - 1 - event.button.y);
+					break;
+				case SDL_MOUSEBUTTONUP:
+					mousePressed = false;
+				default:
+					break;
+				}
+			}
+
+			bool doFlip = false;
+			{
+				ScopedLock lock(g_frameHandshakeMutex);
+				if (g_quit)
+				{
+					if (g_frameReady)
+					{
+						// unlock waiting thread
+						SDL_CondSignal(m_frameReceivedEvent.get());
+					}
+				}
+				// if either the flag is set or variable has been signaled do the blit
+				else if (blitNow || g_frameReady || SDL_CondWaitTimeout(m_frameReadyEvent.get(), g_frameHandshakeMutex.get(), 33) == 0)
+				{
+					doFlip = true;
+					SDL_BlitSurface(g_surface.get(), NULL, screen, NULL);
+
+					if (pendingResize)
+					{
+						resizeOrCreateSurface(screen->w, screen->h);
+						pendingResize = false;
+					}
+
+					if (!blitNow || g_frameReady)
+					{
+						// transfer variables (resolution is transfered elsewhere)
+						glsl_sandbox::time = time;
+						glsl_sandbox::mouse = mousePosition;
+						// reset flags
+						g_cancelDraw = g_frameReady = false;
+						SDL_CondSignal(m_frameReceivedEvent.get());
+					}
+				}
+			}
+
+			if (doFlip)
+			{
+				++frame;
+				SDL_Flip(screen);
+#ifdef WRITE_GIF
+				GifWriteFrame(&gif, reinterpret_cast<const uint8_t *>(screen->pixels), screen->w, screen->h, 100 / 33);
+#endif
+			}
+
+			cout << "frame: " << frame << "\t time: " << time << "\t timescale: " << timeScale << "         \r";
+			cout.flush();
+
+			clock_t delta = clock() - begin;
+			time += static_cast<float>(delta / double(CLOCKS_PER_SEC) * timeScale);
+			begin = clock();
+		}
+
+		// wait for the render thread to stop
+		cout << "\nwaiting for the worker thread to finish...";
+		SDL_WaitThread(renderThreadInstance, nullptr);
+	}
+	catch (exception& error)
+	{
+		cerr << "ERROR: " << error.what() << endl;
+	}
+	catch (...)
+	{
+		cerr << "ERROR: Unknown error" << endl;
+	}
+
+	SDL_Quit();
+
+#ifdef WRITE_GIF
+	GifEnd(&gif);
+#endif
+
+	return 0;
+}
+
+
+sampler2D::sampler2D(const char* path, WrapMode wrapMode) : m_wrapMode(wrapMode)
+{
+	m_image = IMG_Load(path);
+	if (!m_image)
+	{
+		std::cerr << "WARNING: Failed to load texture " << path << "\n";
+		std::cerr << "  SDL_Image message: " << IMG_GetError() << "\n";
+	}
+}
+
+sampler2D::~sampler2D()
+{
+	if (m_image)
+	{
+		SDL_FreeSurface(m_image);
+		m_image = nullptr;
+	}
+}
+
+vec4 sampler2D::sample(vec2 coord)
+{
+	using namespace glsl_sandbox;
+
+	switch (m_wrapMode)
+	{
+	case Repeat:
+		coord = mod(coord, 1);
+		break;
+	case MirrorRepeat:
+		coord = abs(mod(coord - 1, 2) - 1);
+		break;
+	case Clamp:
+	default:
+		coord = clamp(coord, 0, 1);
+		break;
+	}
+
+	// OGL uses left-bottom corner as origin...
+	coord.y = 1 - coord.y;
+
+	if (!m_image)
+	{
+		// checkers
+		if (coord.x < 0.5 && coord.y < 0.5 || coord.x > 0.5 && coord.y > 0.5)
+		{
+			return vec4(1, 0, 0, 1);
+		}
+		else
+		{
+			return vec4(0, 1, 0, 1);
+		}
+	}
+	else
+	{
+		int x = static_cast<int>(coord.x * (m_image->w - 1) + 0.5);
+		int y = static_cast<int>(coord.y * (m_image->h - 1) + 0.5);
+
+		auto& format = *m_image->format;
+		auto pixelPtr = static_cast<uint8_t*>(m_image->pixels) + (y * m_image->pitch + x * format.BytesPerPixel);
+		uint32_t pixel = 0;
+		for (size_t i = 0; i < format.BytesPerPixel; ++i)
+		{
+			pixel |= (pixelPtr[i] << (i * 8));
+		}
+
+		int r = (pixel & format.Rmask) >> format.Rshift;
+		int g = (pixel & format.Gmask) >> format.Gshift;
+		int b = (pixel & format.Bmask) >> format.Bshift;
+		int a = format.Amask ? ((pixel & format.Amask) >> format.Ashift) : 255;
+		return clamp(vec4(r, g, b, a) / 255.0f, 0.0f, 1.0f);
+	}
+}
---
+++ src/egg.h
@@ -0,0 +1,396 @@
+#ifdef __cplusplus
+#define SCREEN_WIDTH 100
+#define SCREEN_HEIGHT 100
+#define _in(T) const T &
+#define _inout(T) T &
+#define _out(T) T &
+#define _begin {
+#define _end }
+#else
+#define _in(T) const in T
+#define _inout(T) inout T
+#define _out(T) out T
+#define _begin (
+#define _end )
+#endif
+
+struct ray_t {
+	vec3 origin;
+	vec3 direction;
+};
+
+struct hit_t {
+	float t;
+	int material_id;
+	float material_param;
+	vec3 normal;
+	vec3 origin;
+};
+
+#define max_dist 999.0
+hit_t no_hit = hit_t _begin
+	(max_dist + 1.0), -1, 1., vec3(0), vec3(0)
+_end;
+
+mat3 rotate_around_z(_in(float) angle_degrees);
+mat3 rotate_around_y(_in(float) angle_degrees);
+mat3 rotate_around_x(_in(float) angle_degrees);
+vec3 corect_gamma(_in(vec3) color);
+ray_t get_primary_ray(_in(vec3) cam_local_point, _in(vec3) cam_origin, _in(vec3) cam_look_at);
+vec3 ik_solver(vec3 start, vec3 goal, float bone_length_1, float bone_length_2);
+
+vec3 background(_in(ray_t) ray)
+{
+	return vec3(.1, .1, .7);
+}
+
+void setup_scene()
+{
+#define mat_debug 0
+#define mat_egg 1
+#define mat_bike 2
+#define mat_ground 3
+}
+
+vec2 op_add(vec2 d1, vec2 d2) // union
+{
+	// minimum distance
+	return d1.x < d2.x ? d1 : d2;
+}
+
+float op_sub(float d1, float d2) // difference
+{
+	// TODO: see original paper
+	// diff between first and complement of the second fields
+	// aka the second 'carved out' from the first
+	return max(d1, -d2);
+}
+
+float op_intersect(float d1, float d2)
+{
+	// TODO: see original paper
+	// what's common for both fields
+	return max(d1, d2);
+}
+
+float sd_plane(vec3 p, vec3 n, float d)
+{
+	// distance from point to plane
+	// http://mathworld.wolfram.com/Point-PlaneDistance.html
+	return dot(n, p) + d;
+}
+
+float sd_sphere(vec3 p, float s)
+{
+	// distance to center of sphere offset by the radius
+	return length(p) - s;
+}
+
+float sd_box(vec3 p, vec3 b)
+{
+	// intersection of 3 axis aligned 'slabs'
+	return max(abs(p.x) - b.x, max(abs(p.y) - b.y, abs(p.z) - b.z));
+}
+
+float sd_torus(vec3 p, vec2 t) // around y axis
+{
+	// TODO: visualise this
+	return length(vec2(length(p.xz) - t.x, p.y)) - t.y;
+}
+
+float sd_y_cylinder(vec3 p, vec2 c)
+{
+	// distance to the Y axis, offset (aka inflated) by the cylinder radius
+	// then intersected with 2 cutting planes
+	return max(length(p.xz) - c.r, abs(p.y) - c.g / 2.);
+}
+
+float sd_cylinder(vec3 P, vec3 P0, vec3 P1, float R)
+{
+	// distance to segment -- http://geomalgorithms.com/a02-_lines.html
+	// then cut it with 2 planes at the ends
+	// then offset it with radius    
+	vec3 dir = normalize(P1 - P0);
+	float dist = length(cross(dir, P - P0));
+	float plane_1 = sd_plane(P, dir, length(P1));
+	float plane_2 = sd_plane(P, -dir, -length(P0));
+	return op_sub(op_sub(dist, plane_1), plane_2) - R;
+}
+
+float op_blend(float a, float b, float k)
+{
+	// esentially liniar blend but more fancy
+	// from http://iquilezles.org/www/articles/smin/smin.htm
+	// NOTE: not true distance but estimate
+	float h = clamp(0.5 + 0.5*(b - a) / k, 0.0, 1.0);
+	return mix(b, a, h) - k*h*(1.0 - h);
+}
+
+vec2 sdf(_in(vec3) P)
+{
+	vec3 p = rotate_around_y(iGlobalTime * -50.0) * P -
+		vec3(0, 0.5, 1.75);
+
+	int material = mat_egg;
+
+	float egg_y = 0.65;
+	float egg_m = sd_sphere(p - vec3(0, egg_y, 0), 0.475);
+	float egg_b = sd_sphere(p - vec3(0, egg_y - 0.45, 0), 0.25);
+	float egg_t = sd_sphere(p - vec3(0, egg_y + 0.45, 0), 0.25);
+	float egg_1 = op_blend(egg_m, egg_b, .5);
+	float egg_2 = op_blend(egg_1, egg_t, .5);
+	vec2 egg = vec2(egg_2, material);
+
+	vec3 wheel_pos = vec3(0, 1.2, 0);
+	float pedal_radius = 0.3;
+	float pedal_speed = 300.;
+	float pedal_off = 0.2;
+
+	mat3 rot_z = rotate_around_z(-iGlobalTime * pedal_speed);
+	vec3 left_foot_pos = wheel_pos + rot_z * vec3(0, pedal_radius, pedal_off);
+
+	rot_z = rotate_around_z(-iGlobalTime * pedal_speed);
+	vec3 right_foot_pos = wheel_pos + rot_z * vec3(0, -pedal_radius, -pedal_off);
+
+	vec3 side = vec3(0, 0, pedal_off);
+	float femur = 0.8;
+	float tibia = 0.75;
+	float thick = .05;
+
+	vec3 pelvis = vec3(0, 0., 0) + side;
+	vec3 knee_l = ik_solver(pelvis, left_foot_pos, femur, tibia);
+	vec2 left_leg_a = vec2(
+		sd_cylinder(p + pelvis, vec3(0), knee_l - side, thick),
+		material);
+	vec2 left_leg_b = vec2(
+		sd_cylinder(p + knee_l, vec3(0), left_foot_pos - knee_l, thick),
+		material);
+
+	pelvis = vec3(0, 0., 0) - side;
+	vec3 knee_r = ik_solver(pelvis, right_foot_pos, femur, tibia);
+	vec2 right_leg_a = vec2(
+		sd_cylinder(p + pelvis, vec3(0), knee_r + side, thick),
+		material);
+	vec2 right_leg_b = vec2(
+		sd_cylinder(p + knee_r, vec3(0), right_foot_pos - knee_r, thick),
+		material);
+
+	vec2 legs = op_add(
+		vec2(op_blend(left_leg_a.x, left_leg_b.x, .01), material),
+		op_add(right_leg_a, right_leg_b));
+
+	vec3 left_toe = normalize(vec3(left_foot_pos.y - knee_l.y, knee_l.x - left_foot_pos.x, 0));
+	vec2 left_foot = vec2(
+		//sd_sphere(p + left_foot_pos, 0.1),
+		sd_cylinder(p + left_foot_pos, vec3(0), left_toe / 8., thick),
+		material);
+
+	vec3 right_toe = normalize(vec3(right_foot_pos.y - knee_r.y, knee_r.x - right_foot_pos.x, 0));
+	vec2 right_foot = vec2(
+		//sd_sphere(p + right_foot_pos, 0.1),
+		sd_cylinder(p + right_foot_pos, vec3(0), right_toe / 8., thick),
+		material);
+
+	vec2 feet = op_add(left_foot, right_foot);
+
+	mat3 rot = rotate_around_x(90.);
+	vec2 bike = vec2(
+		sd_torus(rot * (p + wheel_pos), vec2(1., .025)),
+		mat_bike);
+
+	vec2 ground = vec2(
+		sd_plane(P, vec3(0, 1, 0), wheel_pos.y + 0.5),
+		mat_ground);
+
+	// debug
+	//vec2 origin = vec2(sd_sphere(p, .1), mat_debug);
+	//vec2 kr = vec2(sd_sphere(p + knee_r, .1), mat_debug);
+	//vec2 debug = origin;//op_add(origin, debug_light);
+
+	return op_add(
+		ground,
+		op_add(legs, op_add(egg, op_add(feet, bike))));
+}
+
+vec3 sdf_normal(_in(vec3) p)
+{
+	float dt = 0.05;
+	vec3 x = vec3(dt, 0, 0);
+	vec3 y = vec3(0, dt, 0);
+	vec3 z = vec3(0, 0, dt);
+	return normalize(vec3(
+		sdf(p + x).r - sdf(p - x).r,
+		sdf(p + y).r - sdf(p - y).r,
+		sdf(p + z).r - sdf(p - z).r
+		));
+}
+
+float shadowmarch(_in(ray_t) ray) // from http://iquilezles.org/www/articles/rmshadows/rmshadows.htm
+{
+	const int steps = 30;
+	const float penumbra_factor = 48.;
+
+	float t = 0.;
+	float umbra = 1.;
+
+	for (int i = 0; i < steps; i++) {
+		vec3 p = ray.origin + ray.direction * t;
+		vec2 d = sdf(p);
+
+		if (t > 10.) break;
+		if (d.x < 0.001) {
+			return 0.1;
+		}
+
+		t += d.x;
+		umbra = min(umbra, penumbra_factor * d.x / t);
+	}
+
+	return umbra;
+}
+
+vec3 raymarch(_in(ray_t) ray)
+{
+	float t = 0.;
+	const int steps = 50;
+
+	for (int i = 0; i < steps; i++) {
+		vec3 p = ray.origin + ray.direction * t;
+		vec2 d = sdf(p);
+
+		if (t > 10.) break;
+		if (d.x < 0.01) {
+			return vec3(float(i) / float(steps));
+		}
+
+		t += d.x;
+	}
+
+	return background(ray);
+}
+
+void mainImage(_out(vec4) fragColor, _in(vec2) fragCoord)
+{
+	// assuming screen width is larger than height 
+	vec2 aspect_ratio = vec2(iResolution.x / iResolution.y, 1);
+	// field of view
+	float fov = tan(radians(30.0));
+
+	// antialising
+#if 0
+#define MSAA_PASSES 4
+	float offset = 0.25;
+	float ofst_x = offset * aspect_ratio.x;
+	float ofst_y = offset;
+	vec2 msaa[MSAA_PASSES];
+	msaa[0] = vec2(-ofst_x, -ofst_y);
+	msaa[1] = vec2(-ofst_x, +ofst_y);
+	msaa[2] = vec2(+ofst_x, -ofst_y);
+	msaa[3] = vec2(+ofst_x, +ofst_y);
+#else
+#define MSAA_PASSES 1
+	vec2 msaa[MSAA_PASSES];
+	msaa[0] = vec2(0.5);
+#endif
+
+	vec3 eye = vec3(1, 2, 5);
+	vec3 look_at = vec3(0);
+
+	vec3 color = vec3(0);
+
+	for (int i = 0; i < MSAA_PASSES; i++) {
+		vec2 point_ndc = (fragCoord.xy + msaa[i]) / iResolution.xy;
+		vec3 point_cam = vec3((2.0 * point_ndc - 1.0) * aspect_ratio * fov, -1.0);
+
+		ray_t ray = get_primary_ray(point_cam, eye, look_at);
+
+		color += raymarch(ray) / float(MSAA_PASSES);
+	}
+
+	fragColor = vec4(corect_gamma(color), 1);
+}
+
+mat3 rotate_around_z(_in(float) angle_degrees)
+{
+	float angle = radians(angle_degrees);
+	float _sin = sin(angle);
+	float _cos = cos(angle);
+	return mat3(_cos, -_sin, 0, _sin, _cos, 0, 0, 0, 1);
+}
+
+mat3 rotate_around_y(_in(float) angle_degrees)
+{
+	float angle = radians(angle_degrees);
+	float _sin = sin(angle);
+	float _cos = cos(angle);
+	return mat3(_cos, 0, _sin, 0, 1, 0, -_sin, 0, _cos);
+}
+
+mat3 rotate_around_x(_in(float) angle_degrees)
+{
+	float angle = radians(angle_degrees);
+	float _sin = sin(angle);
+	float _cos = cos(angle);
+	return mat3(1, 0, 0, 0, _cos, -_sin, 0, _sin, _cos);
+}
+
+vec3 corect_gamma(_in(vec3) color)
+{
+	float gamma = 1.0 / 2.25;
+	return vec3(pow(color.r, gamma), pow(color.g, gamma), pow(color.b, gamma));
+}
+
+ray_t get_primary_ray(_in(vec3) cam_local_point, _in(vec3) cam_origin, _in(vec3) cam_look_at)
+{
+	vec3 fwd = normalize(cam_look_at - cam_origin);
+	vec3 up = vec3(0, 1, 0);
+	vec3 right = cross(up, fwd);
+	up = cross(fwd, right);
+
+	return ray_t _begin
+		cam_origin,
+		normalize(fwd + up * cam_local_point.y + right * cam_local_point.x)
+		_end;
+}
+
+vec3 ik_2_bone_centered_solver(vec3 goal, float L1, float L2)
+{
+#if 0 // from https://www.shadertoy.com/view/ldlGR7
+	vec3 q = goal*(0.5 + 0.5*(L1*L1 - L2*L2) / dot(goal, goal));
+
+	float s = L1*L1 - dot(q, q);
+	s = max(s, 0.0);
+	q += sqrt(s)*normalize(cross(goal, vec3(0, 0, 1)));
+
+	return q;
+#else // naive version with law of cosines
+	float G = length(goal);
+
+	// tetha is the angle between bone1 and goal direction
+	// get it from law of cosines applied to the
+	// triangle with sides: bone1, bone2, pivot_of_bone1<->goal
+	float cos_theta = (L1*L1 + G*G - L2*L2) / (2.*L1*G);
+
+	// sin^2 + cos^2 = 1 (Pythagoras in unit circle)
+	float sin_theta = sqrt(1. - cos_theta * cos_theta);
+
+	// rotation matrix by theta amount around the axis
+	// perpendicular to the plane created by bone1 and bone2
+	mat3 rot = mat3(
+		cos_theta, -sin_theta, 0,
+		sin_theta, cos_theta, 0,
+		0, 0, 1.
+		);
+
+	// get the end of bone1 aka the pivot of bone2
+	// by getting a vector from the goal direction
+	// and rotating along with the newly found theta angle
+	return rot * (normalize(goal) * L1);
+#endif
+}
+
+vec3 ik_solver(vec3 start, vec3 goal, float bone_length_1, float bone_length_2)
+{
+	return start + ik_2_bone_centered_solver(
+		goal - start, bone_length_1, bone_length_2);
+}
