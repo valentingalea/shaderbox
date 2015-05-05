5fc11a14746aa26655a00f2278abb4db4fecc752
Valentin Galea <valentingalea@gmail.com>
2015-05-06 00:04:08 +0100

    animated scrolling tunnel

--- src/app_2d.h
+++ src/app_2d.h
@@ -1,6 +1,8 @@
 #include "def.h"
 
-sampler2D u_tex ("", sampler2D::Repeat);
+sampler2D iChannel0 ("", sampler2D::Repeat);
+
+#define u_tex iChannel0
 #define u_res iResolution
 #define u_time iGlobalTime
 
@@ -20,9 +22,13 @@ vec2 perturb (_in(vec2) uv)
 	float r = sqrt (dot (p, p));
 	float a = atan (p.y, p.x);
 	
-	float s = cos (a) / r;
-	float t = sin (a) / r;
-	
+//	float s = p.x*cos(2.*r) - p.y*sin(2.*r);
+//	float t = p.x*cos(2.*r) - p.y*sin(2.*r);
+//  float s = 1./(r+0.5+0.5*sin(5.*a));
+//	float t = a*3./PI;
+	float s = p.x / abs(p.y);
+	float t = 1. / abs(p.y);
+
 	return vec2 (mod (s, 1.), mod (t, 1.));
 }
 
@@ -31,8 +37,8 @@ void mainImage(_out(vec4) fragColor, _in(vec2) fragCoord)
 	vec2 uv = fragCoord / u_res.xy;
 	
 	vec4 color = sample (
-	perturb (uv) * 1.
-	//+ u_time
+		perturb (uv)
+		+ vec2(0, u_time)
 	);
 	
 	fragColor = color;
