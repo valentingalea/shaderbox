962f3d69ac586c7a7303a8fe22fe6327ad6b6042
root <root@localhost>
2015-05-05 21:32:44 +0100

    starting point for plane effects

--- src/app_2d.h
+++ src/app_2d.h
@@ -1,6 +1,39 @@
 #include "def.h"
 
+sampler2D u_tex ("", sampler2D::Repeat);
+#define u_res iResolution
+#define u_time iGlobalTime
+
+vec4 sample (_in(vec2) uv)
+{
+#if 1
+	return texture (u_tex, uv);
+#else
+// TODO: some procedural shit
+#endif
+}
+
+vec2 perturb (_in(vec2) uv)
+{
+	vec2 p = 2.*uv - 1.;
+	
+	float r = sqrt (dot (p, p));
+	float a = atan (p.y, p.x);
+	
+	float s = cos (a) / r;
+	float t = sin (a) / r;
+	
+	return vec2 (mod (s, 1.), mod (t, 1.));
+}
+
 void mainImage(_out(vec4) fragColor, _in(vec2) fragCoord)
 {
-	fragColor = vec4 (1, 0, 0, 1);
+	vec2 uv = fragCoord / u_res.xy;
+	
+	vec4 color = sample (
+	perturb (uv) * 1.
+	//+ u_time
+	);
+	
+	fragColor = color;
 }
