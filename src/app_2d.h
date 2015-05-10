d9e960087873b6ba79f45b86a2fddd5cd6fbd79f
root <root@localhost>
2015-05-10 10:12:50 +0100

    tunnel effect; refactor

--- src/app_2d.h
+++ src/app_2d.h
@@ -9,7 +9,7 @@ sampler2D iChannel0 ("", sampler2D::Repeat);
 
 vec4 sample (_in(vec2) uv)
 {
-#if 0
+#if 1
 	return texture (u_tex, uv);
 #else
 	float n = fbm (uv, 0.5, 0.5, 2., 2., 6);
@@ -17,21 +17,27 @@ vec4 sample (_in(vec2) uv)
 #endif
 }
 
-vec2 perturb (_in(vec2) uv)
+vec2 perturb_road (_in(vec2) uv)
 {
 	vec2 p = 2.*uv - 1.;
 	
-	float r = sqrt (dot (p, p));
-	float a = atan (p.y, p.x);
-	
-//	float s = p.x*cos(2.*r) - p.y*sin(2.*r);
-//	float t = p.x*cos(2.*r) - p.y*sin(2.*r);
-//  float s = 1./(r+0.5+0.5*sin(5.*a));
-//	float t = a*3./PI;
 	float s = p.x / abs(p.y);
 	float t = 1. / abs(p.y);
 
-	return vec2 (mod (s, 1.), mod (t, 1.));
+	return  vec2 (s, t);
+}
+
+vec2 perturb_tunnel (_in(vec2) uv, _inout(float) r)
+{
+	vec2 p = 2.*uv - 1.;
+	
+	r = sqrt (dot (p, p));
+	float a = atan (p.y, p.x) + u_time;
+	
+	float s = 1. / r + u_time;
+	float t = 4. * (a / PI);
+
+	return  vec2 (s, t);
 }
 
 float tent_filter (float t) // -1 to 1, peak at 0
@@ -42,16 +48,21 @@ float tent_filter (float t) // -1 to 1, peak at 0
 void mainImage(_out(vec4) fragColor, _in(vec2) fragCoord)
 {
 	vec2 uv = fragCoord / u_res.xy;
-/*	
-	vec4 color = sample (
-		perturb (uv)
-		+ vec2(0, u_time)
-	);
 	
-	color *= 1. - tent_filter (2.*uv.y - 1.);
-*/
+#if 1
+	float r = 1./8.;
+	uv.x += r * cos (u_time * 1.);
+	uv.y += r * sin (u_time * 1.);
+	
+	float d;
+	vec2 st = perturb_tunnel (uv, d);
+	vec4 color = sample (st) * d;
+#else
+	vec2 st = perturb_road (uv);
+	vec4 color = sample (st + vec2(0, u_time));
 
-	vec4 color = sample (uv / 4.);
+	color *= 1. - tent_filter (2.*uv.y - 1.);
+#endif
 		
 	fragColor = color;
 }
