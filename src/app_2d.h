9ec98a83f348270e931f4f6639211c312060bd2a
root <root@localhost>
2015-05-14 09:10:12 +0100

    combined 2 effects

--- src/app_2d.h
+++ src/app_2d.h
@@ -17,24 +17,29 @@ vec4 sample (_in(vec2) uv)
 #endif
 }
 
-vec2 perturb_road (_in(vec2) uv)
+vec2 perturb_road (
+_in(vec2) uv,
+_in(float) time)
 {
 	vec2 p = 2.*uv - 1.;
 	
 	float s = p.x / abs(p.y);
 	float t = 1. / abs(p.y);
 
-	return  vec2 (s, t);
+	return  vec2 (s, t - time);
 }
 
-vec2 perturb_tunnel (_in(vec2) uv, _inout(float) r)
+vec2 perturb_tunnel (
+_in(vec2) uv,
+_in(float) time,
+_inout(float) r)
 {
 	vec2 p = 2.*uv - 1.;
 	
 	r = sqrt (dot (p, p));
-	float a = atan (p.y, p.x) + u_time;
+	float a = atan (p.y, p.x) + time;
 	
-	float s = 1. / r + u_time;
+	float s = 1. / r + time;
 	float t = 4. * (a / PI);
 
 	return  vec2 (s, t);
@@ -49,20 +54,37 @@ void mainImage(_out(vec4) fragColor, _in(vec2) fragCoord)
 {
 	vec2 uv = fragCoord / u_res.xy;
 	
-#if 1
-	float r = 1./8.;
-	uv.x += r * cos (u_time * 1.);
-	uv.y += r * sin (u_time * 1.);
+	vec2 st;
+	vec4 color;
+	float d = 1.;
+	float t = mod (u_time, 16.);
 	
-	float d;
-	vec2 st = perturb_tunnel (uv, d);
-	vec4 color = sample (st) * d;
-#else
-	vec2 st = perturb_road (uv) + vec2(0, u_time);
-	vec4 color = sample (st);
+	if (t < 4.) {
+		st = perturb_tunnel (uv, u_time, d);
+		color = sample (st);
+		color *= d;
+	}
+	
+	if (t > 4. && t < 8.) {
+		st = perturb_tunnel (uv, 1., d);
+		vec2 st2 = perturb_road (uv, 1.);
+		color = sample (mix (st, st2, (t - 4.) / 4.));
+		color *= d;	
+	}
+	
+	if (t > 8. && t < 12.) {
+		st = perturb_road (uv, u_time);
+		color = sample (st);
+	}
 
+	if (t > 12.) {
+		st = perturb_tunnel (uv, 1., d);
+		vec2 st2 = perturb_road (uv, 1.);
+		color = sample (mix (st2, st, (t - 12.) / 4.));
+		color *= d;	
+	}
+	
 	color *= 1. - tent_filter (2.*uv.y - 1.);
-#endif
 
 	// debug
 	//color = vec4 (vec2 (fmod (st.x, 1.), fmod (st.y, 1.)),0, 1.);
