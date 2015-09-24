b74994ff28f28891f5a3e83fe8f79432d192bac0
root <root@localhost>
2015-09-24 20:08:42 +0100

    refactor of the global defs

--- src/def.h
+++ src/def.h
@@ -1,3 +1,4 @@
+// GLSL/C++ compatibility layer
 #ifdef __cplusplus
 #define _in(T) const T &
 #define _inout(T) T &
@@ -12,25 +13,30 @@
 #define _end )
 #endif
 
+#define PI 3.14159265359
+
+// Shadertoy specific uniforms
+#define u_res iResolution
+#define u_time iGlobalTime
+#define u_tex0 iChannel0
+
 struct ray_t {
 	vec3 origin;
 	vec3 direction;
 };
+#define BIAS 1e-4 // small offset to avoid self-intersections
 
 struct hit_t {
 	float t;
 	int material_id;
-	float material_param; // used by dynamic materials to store temp data
+	float material_param;
 	vec3 normal;
 	vec3 origin;
 };
-
-#define max_dist 999.0
+#define max_dist 1e9 // TODO: precision issues
 hit_t no_hit = hit_t _begin
-	(max_dist + 1.0), -1, 1., vec3(0), vec3(0)
+	(max_dist + 1e1), -1, 1., vec3(0), vec3(0)
 _end;
 
-#define BIAS 1e-4 // small offset to add to ray when retracing to avoid self-intersection
-#define PI 3.14159265359
-
+// camera system
 vec3 eye, look_at;
