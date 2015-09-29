9af306e17a82b425c6b2f89ab2a72b4571a6c035
Valentin Galea <valentingalea@gmail.com>
2015-09-29 12:35:35 +0100

    noise lib improv: detached func; unrolled fbm; changed to 3d

--- src/noise.h
+++ src/noise.h
@@ -1,10 +1,12 @@
 // ----------------------------------------------------------------------------
 // Noise functions
 // ----------------------------------------------------------------------------
-#include "../lib/ashima-noise/src/noise2d.glsl"
+#include "../lib/ashima-noise/src/noise3d.glsl"
 
-float fbm (
-	_in(vec2) uv,
+#define NOISE_FUNC snoise
+
+float fbm_generic (
+	_in(vec3) pos,
 	_in(float) start_amplitude,
 	_in(float) gain,
 	_in(float) start_frequency,
@@ -16,10 +18,29 @@ float fbm (
 	float frequency = start_frequency;
 	
 	for (int i = 0; i < octaves; i++) {
-		total += amplitude * snoise (uv * frequency);
+		total += amplitude * NOISE_FUNC (pos * frequency);
 		amplitude *= gain;
 		frequency *= lacunarity;
 	}
 	
 	return total;
 }
+
+float fbm_4(
+	_in(vec3) pos	
+){
+	float t = 0.;
+	float p = pos;
+	
+	t  = 0.5000 * NOISE_FUNC(p); p *= 2.001;
+	t += 0.2500 * NOISE_FUNC(p); p *= 2.002;
+	t += 0.1250 * NOISE_FUNC(p); p *= 2.003;
+	t += 0.0625 * NOISE_FUNC(p);
+	
+	return t;
+}
+
+//TODO:
+// - add rotation matrix for extra variation
+// - for the unrolled on input more random amplitudes
+// - domain distortion?
