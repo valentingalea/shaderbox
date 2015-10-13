fdc5478c3e22b461942c64b6e51a95d5f9aacec6
root <root@localhost>
2015-10-13 09:52:41 +0100

    refactor to use includes

--- src/app_terrain.h
+++ src/app_terrain.h
@@ -4,57 +4,9 @@
 // http://www.metoffice.gov.uk/learning/clouds/cloud-spotting-guide
 // http://www.srh.noaa.gov/srh/jetstream/clouds/cloudwise/types.html
 
-// TODO: replace
-float hash( float n ) { return fract(sin(n)*753.5453123); }
-float noise( in vec3 x )
-{
-    vec3 p = floor(x);
-    vec3 f = fract(x);
-    f = f*f*(3.0-2.0*f);
-	
-    float n = p.x + p.y*157.0 + 113.0*p.z;
-    return mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
-                   mix( hash(n+157.0), hash(n+158.0),f.x),f.y),
-               mix(mix( hash(n+113.0), hash(n+114.0),f.x),
-                   mix( hash(n+270.0), hash(n+271.0),f.x),f.y),f.z);
-}
-float fbm(vec3 pos)
-{
-    float f = 0.;
-    vec3 q = pos;
-    f  = 0.5000*noise( q ); q = q*2.01;
-    f += 0.2500*noise( q ); q = q*2.02;
-    f += 0.1250*noise( q ); q = q*2.03;
-    f += 0.0625*noise( q ); q = q*2.01;    
-    return f;
-}
-
-// GLSL/C++ compatibility layer
-#ifdef __cplusplus
-#define _in(T) const T &
-#define _inout(T) T &
-#define _out(T) T &
-#define _begin {
-#define _end }
-#else
-#define _in(T) const in T
-#define _inout(T) inout T
-#define _out(T) out T
-#define _begin (
-#define _end )
-#endif
-
-#define PI 3.14159265359
-
-// Shadertoy specific uniforms
-#define u_res iResolution
-#define u_time iGlobalTime
-#define u_mouse iMouse
-
-struct ray_t {
-	vec3 origin;
-	vec3 direction;
-};
+#include "def.h"
+#include "noise_iq.h"
+#include "fbm.h"
 
 ray_t get_primary_ray(_in(vec3) cam_local_point, _inout(vec3) cam_origin, _inout(vec3) cam_look_at)
 {
@@ -69,17 +21,7 @@ ray_t get_primary_ray(_in(vec3) cam_local_point, _inout(vec3) cam_origin, _inout
 	_end;
 }
 
-mat3 rotate_around_x(_in(float) angle_degrees)
-{
-	float angle = radians(angle_degrees);
-	float _sin = sin(angle);
-	float _cos = cos(angle);
-	return mat3(1, 0, 0, 0, _cos, -_sin, 0, _sin, _cos);
-}
-
-vec3 sun_dir = vec3(0, 1, 0);
-
-vec3 render_sky_color(_in(ray_t) eye)
+vec3 render_sky_color(_in(ray_t) eye, _in(vec3) sun_dir)
 {
     float sun_dot = clamp(dot(eye.direction, sun_dir), 0., 1.);
     
@@ -113,8 +55,8 @@ vec3 render_clouds(_in(ray_t) eye)
     	
     	// density func
     	// layer of cloulds + coverage
-    	float dens = fbm(sample * .9242 + u_time);
-		dens = smoothstep(.33, 1., dens);
+    	float dens = fbm(sample * .9242 + u_time / 4);
+		dens = smoothstep(.533, 1., dens);
 		
 		// coloring
 		src = vec4 (vec3 (1), dens);
@@ -152,15 +94,16 @@ void mainImage(_out(vec4) fragColor, _in(vec2) fragCoord)
 	vec3 point_cam = vec3((2.0 * point_ndc - 1.0) * aspect_ratio * fov, -1.0);
 
 	vec3 col = vec3(0);
-
-	mat3 rot = rotate_around_x(-abs(sin(u_time / 2.)) * 90.);
-	sun_dir *= rot;
+	
+	vec3 sun_dir = vec3(0, 0, -1);
+//	mat3 rot = rotate_around_x(-abs(sin(u_time / 2.)) * 90.);
+//	sun_dir *= rot;
     
     vec3 eye = vec3 (0, 1., 0);
     vec3 look_at = vec3 (0, 1.5, -1);
 	ray_t eye_ray = get_primary_ray(point_cam, eye, look_at);
     
-    col += render_sky_color(eye_ray);
+    col += render_sky_color(eye_ray, sun_dir);
     col += render_clouds(eye_ray);
       
     fragColor = vec4(col, 1);
