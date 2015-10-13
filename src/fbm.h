a4ee4e6a162fef06187188953744f61e1679822c
root <root@localhost>
2015-10-13 09:54:25 +0100

    separate general purpose file for fbm

---
+++ src/fbm.h
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------
+// Fractal Brownian Motion
+// ----------------------------------------------------------------------------
+
+// general algorithm:
+//	for (int i = 0; i < octaves; i++) {
+//		total += amplitude * noise (pos * frequency);
+//		amplitude *= gain;
+//		frequency *= lacunarity;
+//	}
+	
+float fbm(
+	_in(vec3) pos	
+){
+	vec3 p = pos;
+	float
+	t  = 0.51749673 * noise(p); p *= 2.003;
+	t += 0.25584929 * noise(p); p *= 2.001;
+	t += 0.12527603 * noise(p); p *= 2.002;
+	t += 0.06255931 * noise(p);
+	
+	return t;
+}
