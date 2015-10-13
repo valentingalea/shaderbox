b85de86ad47224800e1bac0ba5f375c01e9d0362
valentingalea <valentingalea@gmail.com>
2015-10-13 23:40:03 +0100

    refactored intersect; moved more into def

--- src/def.h
+++ src/def.h
@@ -26,6 +26,18 @@ struct ray_t {
 };
 #define BIAS 1e-4 // small offset to avoid self-intersections
 
+struct sphere_t {
+	vec3 origin;
+	float radius;
+	int material;
+};
+
+struct plane_t {
+	vec3 direction;
+	float distance;
+	int material;
+};
+
 struct hit_t {
 	float t;
 	int material_id;
@@ -33,9 +45,13 @@ struct hit_t {
 	vec3 normal;
 	vec3 origin;
 };
-#define max_dist 1e8 // TODO: precision issues
+#define max_dist 1e8
 hit_t no_hit = hit_t _begin
-	(max_dist + 1e1), -1, 1., vec3(0), vec3(0)
+	(max_dist + 1e1), // 'infinite' distance
+	-1, // material id
+	0., // material param
+	vec3(0, 0, 0), // normal
+	vec3(0, 0, 0) // origin
 _end;
 
 // camera system
--- src/intersect.h
+++ src/intersect.h
@@ -2,66 +2,38 @@
 // Analytical surface-ray intersection routines
 // ----------------------------------------------------------------------------
 
-struct sphere_t {
-	vec3 origin;
-	float radius;
-	int material;
-};
-
-struct plane_t {
-	vec3 direction;
-	float distance;
-	int material;
-};
-
+// geometrical solution
+// info: http://www.scratchapixel.com/old/lessons/3d-basic-lessons/lesson-7-intersecting-simple-shapes/ray-sphere-intersection/
 void intersect_sphere(
 	_in(ray_t) ray,
 	_in(sphere_t) sphere,
 	_inout(hit_t) hit
 ){
-#if 1
-	// geometrical solution
-	// info: http://www.scratchapixel.com/old/lessons/3d-basic-lessons/lesson-7-intersecting-simple-shapes/ray-sphere-intersection/
 	vec3 rc = sphere.origin - ray.origin;
 	float radius2 = sphere.radius * sphere.radius;
 	float tca = dot(rc, ray.direction);
 	if (tca < 0.) return;
+
 	float d2 = dot(rc, rc) - tca * tca;
 	if (d2 > radius2) return;
+
 	float thc = sqrt(radius2 - d2);
 	float t0 = tca - thc;
 	float t1 = tca + thc;
 
 	if (t0 < 0.) t0 = t1;
-	if (t0 < hit.t) {
-#else // TODO: wrong for some reason... t gets weird values at intersection
-	// analytical solution
-	// based on combining the
-	// sphere eq: (P - C)^2 = R^2
-	// ray eq: P = O + t*D
-	// into a quadratic eq: ax^2 + bx + c = 0
-	// which can be solved by "completing the square" http://www.mathsisfun.com/algebra/completing-square.html
-	// NOTE: be careful about "catastrophic cancellation" http://en.wikipedia.org/wiki/Loss_of_significance
-	vec3 rc = ray.origin - sphere.origin;
-	//	float a = D dot D -- which is 1 because D is normalised
-	float b = 2.0 * dot(rc, ray.direction); // 2 * (O - C) dot D
-	float c = dot(rc, rc) - sphere.radius * sphere.radius; // (O - C)^2 - R^2
-	float discr = b * b - 4.0 * c;
-	float t = -b - sqrt(abs(discr)); // use abs to avoid the check for < 0
+	if (t0 > hit.t) return;
 
-	if (discr > 0.0 && t > 0.0 && t < hit.t) {
-#endif
-		vec3 impact = ray.origin + ray.direction * t0;
+	vec3 impact = ray.origin + ray.direction * t0;
 
-		hit.t = t0;
-		hit.material_id = sphere.material;
-		hit.material_param = 1.;
-		hit.origin = impact;
-		hit.normal = (impact - sphere.origin) / sphere.radius;
-	}
+	hit.t = t0;
+	hit.material_id = sphere.material;
+	hit.material_param = 1.;
+	hit.origin = impact;
+	hit.normal = (impact - sphere.origin) / sphere.radius;
 }
 
-// Plane is define by normal N and distance to origin P0 (which is on the plane itself)
+// Plane is defined by normal N and distance to origin P0 (which is on the plane itself)
 // a plane eq is: (P - P0) dot N = 0
 // which means that any line on the plane is perpendicular to the plane normal
 // a ray eq: P = O + t*D
@@ -73,22 +45,23 @@ void intersect_plane(
 	_inout(hit_t) hit
 ){
 	float denom = dot(p.direction, ray.direction);
-	if (denom > 1e-6)
-	{
-		float t = dot(vec3(p.distance) - ray.origin, p.direction) / denom;
-		if (t >= 0.0 && t < hit.t)
-		{
-			vec3 impact = ray.origin + ray.direction * t;
+	if (denom < 1e-6) return;
 
-			// checkboard pattern			
-			//vec2 pattern = floor (impact.xz * 0.5);
-			//float cb = mod (pattern.x + pattern.y, 2.0);
+	float t = dot(vec3(p.distance) - ray.origin, p.direction) / denom;
+	if (t < 0. || t > hit.t) return;
+	
+	vec3 impact = ray.origin + ray.direction * t;
 
-			hit.t = t;
-			hit.material_id = p.material;
-			hit.material_param = 1.; // cb; // Disabled for now
-			hit.origin = impact;
-			hit.normal = faceforward(p.direction, ray.direction, p.direction);
-		}
-	}
+#if 0 // checkboard pattern			
+	vec2 pattern = floor (impact.xz * 0.5);
+	float cb = mod (pattern.x + pattern.y, 2.0);
+#else
+	float cb = 0.;
+#endif
+
+	hit.t = t;
+	hit.material_id = p.material;
+	hit.material_param = 1.; // cb; // Disabled for now
+	hit.origin = impact;
+	hit.normal = faceforward(p.direction, ray.direction, p.direction);
 }
