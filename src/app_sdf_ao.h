a06a475af0b7650f785248084a56d14ee322cb18
Valentin Galea <valentingalea@gmail.com>
2016-04-13 22:01:57 +0100

    New primitive: quarter of a pipe

--- src/app_sdf_ao.h
+++ src/app_sdf_ao.h
@@ -25,12 +25,18 @@ void setup_scene()
 	materials[mat_debug].ior = 1.;
 	materials[mat_debug].reflectivity = 0.;
 	materials[mat_debug].translucency = 0.;
+
+#define mat_pipe 0
+#define mat_flat 0
+#define mat_bottom 0
+#define mat_deck 0
+#define mat_copping 0
 }
 
 void setup_camera(_inout(vec3) eye, _inout(vec3) look_at)
 {
 	mat3 rot = rotate_around_y (u_time * 50.);
-	eye = mul(rot, vec3(2, 2, 4));
+	eye = mul(rot, vec3(0, 3, 5));
 	look_at = vec3(0, 0, 0);
 }
 
@@ -42,37 +48,51 @@ vec3 illuminate(_in(vec3) eye, _in(hit_t) hit)
 
 	material_t mat = get_material(hit.material_id);
 
-	vec3 accum = ambient_light; // really cheap equivalent for indirect light
-
 	vec3 V = normalize(eye - hit.origin); // view direction
-	vec3 L = normalize (vec3 (1, 1, 0)); //get_light_direction(lights[0], hit);
-
-#if 0
-		accum += illum_blinn_phong(V, L, hit, mat);
-#else
-		accum += illum_cook_torrance(V, L, hit, mat);
-#endif
-
-	return accum;
+	vec3 L = normalize (vec3 (1, 1, 1)); //get_light_direction(lights[0], hit);
+	
+	return max(0., dot(L, hit.normal))
+	* mat.base_color + ambient_light;
 }
 
-vec2 sdf(_in(vec3) p)
+vec2 sdf(_in(vec3) pos)
 {
-	vec2 box = vec2 (
-		sd_box (p + vec3 (1, 0, 0), vec3 (1, 1, 1)),
-		mat_phong);
+	// NOTE: everything is centered around origin
+	// change coord frame by offseting
+	// with inverse then doing
+	// the opposite before next
+	// effectively doing push/pop
+	
+	// NOTE: all measurements are in halfs
+	
+	vec3 p = pos; //- vec3 (0, 1, 0);
+	
+	const vec3 size = vec3 (1, 1, 1.25);
+	
+	p.y -= size.y;
+	float b = sd_box (p, size);
+	
+	p -= vec3 (.65, .5, 0);
+	p = mul (p, rotate_around_x (-90));
+	float c = sd_y_cylinder (p,
+		size.y + .25, // radius
+		2. * size.z + .1); // height
 		
-	vec2 globe = vec2 (
-		sd_sphere (p - vec3 (1, 0, 0), 1.),
-		mat_phong);
-		
+	vec2 pipe = vec2 (
+		op_sub (b, c),
+		mat_pipe);
+
+	vec2 ref = vec2 (
+		sd_box(pos, vec3 (.125, 15, .125)),
+		mat_debug);
+	
 	vec2 ground = vec2 (
-		sd_plane (p, vec3 (0, 1, 0), 1.),
+		sd_plane (pos, vec3 (0, 1, 0), 0.),
 		mat_ground);
 
 	return op_add (
-		op_add (box, globe),
-		ground);
+		pipe,
+		op_add (ground, ref));
 }
 
 vec3 sdf_normal(_in(vec3) p)
@@ -110,16 +130,16 @@ vec3 sdf_ao(_in(hit_t) hit)
 
 vec3 render(_in(ray_t) ray)
 {
-	const int steps = 50;
-	const float end = 10.;
+	const int steps = 70;
+	const float end = 20.;
 
 	float t = 0.;
 	for (int i = 0; i < steps; i++) {
 		vec3 p = ray.origin + ray.direction * t;
 		vec2 d = sdf(p);
 
-		if (t > end) break;
-		if (d.x < EPSILON) {
+		//if (t > end) break;
+		if (d.x < .005) {
 			hit_t h = _begin(hit_t)
 				t, // ray length at impact
 				int(d.y), // material id
