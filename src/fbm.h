1741bf11c3ffd25d5f0369a683721af073821402
valentingalea <valentingalea@gmail.com>
2016-04-18 23:01:49 +0100

    generalized fbm function

--- src/fbm.h
+++ src/fbm.h
@@ -1,17 +1,18 @@
 // ----------------------------------------------------------------------------
 // Fractal Brownian Motion
+// depends on noise() primitive
 // ----------------------------------------------------------------------------
 
-float fbm(
-	_in(vec3) pos,
-	_in(float) lacunarity
-){
-	vec3 p = pos;
-	float
-	t  = 0.51749673 * noise(p); p *= lacunarity;
-	t += 0.25584929 * noise(p); p *= lacunarity;
-	t += 0.12527603 * noise(p); p *= lacunarity;
-	t += 0.06255931 * noise(p);
-	
-	return t;
-}
+#define DECL_FBM_FUNC(_name, _octaves, _gain) \
+	float _name(_in(vec3) pos, _in(float) lacunarity) { \
+		vec3 p = pos; \
+		float gain = _gain; \
+		float t = 0.; \
+		for (int i = 0; i < _octaves; i++) { \
+			t += gain * noise(p); p *= lacunarity; \
+			gain *= .5; \
+		} \
+		return t; \
+	}
+
+DECL_FBM_FUNC(fbm, 4, .5)
