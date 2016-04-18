477a9a400e7f69aa3865a2e65e61c71871fc1dde
valentingalea <valentingalea@gmail.com>
2016-04-18 23:14:50 +0100

    sadly had to colapse fbm macro to one line due to shadertoy/glsl

--- src/fbm.h
+++ src/fbm.h
@@ -3,16 +3,6 @@
 // depends on noise() primitive
 // ----------------------------------------------------------------------------
 
-#define DECL_FBM_FUNC(_name, _octaves, _gain) \
-	float _name(_in(vec3) pos, _in(float) lacunarity) { \
-		vec3 p = pos; \
-		float gain = _gain; \
-		float t = 0.; \
-		for (int i = 0; i < _octaves; i++) { \
-			t += gain * noise(p); p *= lacunarity; \
-			gain *= .5; \
-		} \
-		return t; \
-	}
+#define DECL_FBM_FUNC(_name, _octaves, _gain) float _name(_in(vec3) pos, _in(float) lacunarity) { vec3 p = pos; float gain = _gain; float t = 0.; for (int i = 0; i < _octaves; i++) { t += gain * noise(p); p *= lacunarity; gain *= .5; } return t; }
 
 DECL_FBM_FUNC(fbm, 4, .5)
