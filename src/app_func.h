399be608dcfe43a94aad5e147b36a6518dce32fb
Valentin Galea <valentingalea@gmail.com>
2016-04-28 12:20:08 +0100

    improved func plotting; works on all platforms now

--- src/app_func.h
+++ src/app_func.h
@@ -1,5 +1,19 @@
 #include "def.h"
 
+#define SCALE 4.
+#define D (.0125 * SCALE)
+
+vec3 plot(
+	_in(float) f,
+	_in(float) x,
+	_in(vec3) color
+){
+	float y = smoothstep (f-D, f+D, x);
+	y *= 1.- y;
+	
+	return y * color * 5.;
+}
+
 void mainImage(
 	_out(vec4) fragColor,
 #ifdef SHADERTOY
@@ -8,18 +22,26 @@ void mainImage(
 	_in(vec2) fragCoord
 #endif
 ){
-#define SCALE 4.
-#define D (.025 * SCALE)
+// go from [0..resolution] to [0..1]
+	vec2 t = fragCoord.xy / u_res.xy;
+#ifdef HLSL
+	t.y = 1. - t.y;
+#endif
 
-	vec2 t = fragCoord / u_res;
-	t = (t* 2. - 1.) * SCALE;
-	t.x += u_time * SCALE;
+// optional: center around origin by going to [-1, +1] and scale
+	t = (t * 2. - 1.) * SCALE;
 	
-	float f = sin((t.x));
+// plot the axes
+	vec3 col = vec3(0, 0, 0);
+	col += plot(0., t.y, vec3(1, 1, 1));
+	col += plot(t.x, 0., vec3(1, 1, 1));
 	
-	f = smoothstep (f-D, f+D, t.y);
-	f *= 1.- f;
-	vec3 col = vec3 (f) * vec3 (5);
+// plot custom functions	
+	//t.x += u_time * SCALE;
+	col += plot(sin(t.x), t.y, vec3(1, 0, 0));
+	col += plot(cos(t.x), t.y, vec3(0, 1, 0));
+	col += plot(abs(t.x), t.y, vec3(0, 0, 1));
 	
+// output
 	fragColor = vec4 (col, 1);
 }
