b254421777afcd93ba3836eff116a4825418778b
root <root@localhost>
2016-05-04 10:01:40 +0100

    starting to separate the vol stuff in dedicated file

---
+++ src/volumetric.h
@@ -0,0 +1,18 @@
+//
+// *participating media*
+// for integration purposes we usually consider
+// a differential cylinder filled with a participating medium
+// TODO: drawing
+// TODO: notations
+//
+// *absorbtion*
+//
+// *emission*
+//
+// *scaterring*
+//
+// *phase functions*
+//
+// *tranmission*
+// TODO: beer-lambert law
+// TODO: optical depth
