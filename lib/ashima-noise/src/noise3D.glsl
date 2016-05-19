d8b01695d5bba5b39a9c5219dba8df4a42c1f90f
valentingalea <valentingalea@gmail.com>
2016-05-19 23:38:29 +0100

    fixed for HLSL

--- lib/ashima-noise/src/noise3D.glsl
+++ lib/ashima-noise/src/noise3D.glsl
@@ -77,7 +77,7 @@ float snoise(vec3 v)
   //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
   vec4 s0 = floor(b0)*2.0 + 1.0;
   vec4 s1 = floor(b1)*2.0 + 1.0;
-  vec4 sh = -step(h, vec4(0.0));
+  vec4 sh = -step(h, vec4(0, 0, 0, 0));
 
   vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
   vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
