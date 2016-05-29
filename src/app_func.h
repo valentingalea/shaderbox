aa4c2faedc9b7f17f2e438f117c3149d207f5d14
valentingalea <valentingalea@gmail.com>
2016-05-29 16:23:50 +0100

    cleaned up after accidental commit

--- src/app_func.h
+++ src/app_func.h
@@ -1,18 +1,5 @@
 #include "def.h"
 
-float remap(
-	_in(float) original_value,
-	_in(float) original_min,
-	_in(float) original_max,
-	_in(float) new_min,
-	_in(float) new_max
-) {
-	return new_min +
-		(((original_value - original_min) /
-		(original_max - original_min)) *
-			(new_max - new_min));
-}
-
 #include "../lib/ashima-noise/src/common.glsl"
 #include "../lib/ashima-noise/src/classicnoise3d.glsl"
 #include "../lib/ashima-noise/src/noise3d.glsl"
@@ -20,8 +7,9 @@ float remap(
 #define noise(x) (snoise(x))
 
 #include "fbm.h"
-DECL_FBM_FUNC(fbm_low_freq_perlin, 4, cnoise(p))
-DECL_FBM_FUNC(fbm_low_freq_worley, 3, cellular(p).r)
+DECL_FBM_FUNC(fbm_perlin, 4, cnoise(p))
+DECL_FBM_FUNC(fbm_simplex, 4, snoise(p))
+DECL_FBM_FUNC(fbm_worley, 4, cellular(p).r)
 
 #define SCALE 1.
 #define D (.0125 * SCALE)
@@ -52,16 +40,6 @@ void mainImage(
 #endif
 
 	vec3 col = vec3(0, 0, 0);
-#if 1 // 2D
-
-	vec3 pos = vec3(t, 1);
-	
-	float p = fbm_low_freq_perlin(pos * 4., 2., .5, .5);
-	float w = 1. - fbm_low_freq_worley(pos * 4., 4., .5, .5);
-	float n = remap(p, -w, 1, 0, 1);
-
-	col += vec3(n, n, n);
-#else // 1D
 
 // optional: center around origin by going to [-1, +1] and scale
 	t = (t * 2. - 1.) * SCALE;
@@ -79,12 +57,9 @@ void mainImage(
 	//col += plot(abs(t.x), t.y, vec3(0, 0, 1));
 
 	vec3 pos = vec3(t.x, 0, 0);
-	float p = fbm_low_freq_perlin(pos * 4., 2., .5, .5);
-	float w = 1. - fbm_low_freq_worley(pos * 4., 4., .5, .5);
-	float n = remap(p, -w, 1, 0, 1);
-	
-	col += plot(n, t.y, vec3(0, 0, 1));
-#endif
+	col += plot(fbm_perlin(pos, 2., .5, .5), t.y, vec3(1, 0, 0));
+	col += plot(fbm_simplex(pos, 2., .5, .5), t.y, vec3(0, 1, 0));
+	col += plot(fbm_worley(pos, 2., .5, .5), t.y, vec3(0, 0, 1));
 
 // output
 	fragColor = vec4 (col, 1);
