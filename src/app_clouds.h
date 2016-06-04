7378b2d9ac6b59f54ae829496d7d78e5c4cc1872
valentingalea <valentingalea@gmail.com>
2016-06-04 14:34:35 +0100

    refactor into 'islands' of similar semantic value; merged in the volumetric defines to alllow to change them independently

--- src/app_clouds.h
+++ src/app_clouds.h
@@ -4,52 +4,39 @@
 #define hg_g (.76)
 #include "volumetric.h"
 
-#include "../lib/ashima-noise/src/common.glsl"
-#include "../lib/ashima-noise/src/classicnoise3d.glsl"
-#include "../lib/ashima-noise/src/noise3d.glsl"
-#include "../lib/ashima-noise/src/cellular3d.glsl"
-#define noise(x) (snoise(x))
-
-#include "fbm.h"
-DECL_FBM_FUNC(fbm_simple_but_nice, 5, abs(snoise(p)))
-DECL_FBM_FUNC(fbm_low_freq_perlin, 4, cnoise(p))
-DECL_FBM_FUNC(fbm_low_freq_worley, 3, cellular(p).r)
-
-//#define TEX
-#ifdef TEX
-Texture3D u_tex_noise : register(t1);
-SamplerState u_sampler0 : register(s0);
-#endif
-
-float fbm_final(_in(vec3) pos)
-{
-#ifdef TEX
-	return u_tex_noise.SampleLevel(u_sampler0, pos, 0).r;
-#endif
-
-	//return fbm_simple_but_nice(pos * 2.03, 2.64, .5, .5);
-
-	float p = fbm_low_freq_perlin(pos * 4., 2., .5, .5);
-	float w = 1. - fbm_low_freq_worley(pos * 4., 4., .5, .5);
-	float n = remap(p, -w, 1., 0., 1.);
-	return n;
-}
-
 // ----------------------------------------------------------------------------
-// Clouds
+// Scene
 // ----------------------------------------------------------------------------
-#define cld_march_steps  (50)
-#define cld_coverage     (.3475)
-#define cld_thick        (100.)
-#define cld_absorb_coeff (1.)
-#define cld_wind_dir     vec3(0, 0, -u_time * .2)
-#define cld_sun_dir      normalize(vec3(0, abs(sin(u_time * .3)), -1))
+#define wind_dir	vec3(0, 0, -u_time * .2)
+#define sun_dir		normalize(vec3(0, abs(sin(u_time * .3)), -1))
+#define sun_color	vec3(1., .7, .55)
 
 //#define SPHERE
 #ifdef SPHERE
 _constant(sphere_t) atmosphere = _begin(sphere_t)
 	vec3(0, -475, 0), 500., 0
 _end;
+
+void intersect_sphere(
+	_in(ray_t) ray,
+	_in(sphere_t) sphere,
+	_inout(hit_t) hit
+){
+	vec3 rc = sphere.origin - ray.origin;
+	float radius2 = sphere.radius * sphere.radius;
+	float tca = dot(rc, ray.direction);
+	float d2 = dot(rc, rc) - tca * tca;
+	float thc = sqrt(radius2 - d2);
+	float t0 = tca - thc;
+	float t1 = tca + thc;
+
+	vec3 impact = ray.origin + ray.direction * t0;
+	hit.t = t0;
+	hit.material_id = sphere.material;
+	hit.origin = impact;
+	hit.normal = (impact - sphere.origin) / sphere.radius;
+}
+
 #define cld_noise_factor (1. / atmosphere.radius)
 #else
 #define cld_noise_factor .001
@@ -70,8 +57,7 @@ void setup_scene()
 vec3 render_sky_color(
 	_in(vec3) eye_dir
 ){
-	_constant(vec3) sun_color = vec3(1., .7, .55);
-	float sun_amount = max(dot(eye_dir, cld_sun_dir), 0.);
+	float sun_amount = max(dot(eye_dir, sun_dir), 0.);
 
 	vec3 sky = mix(vec3(.0, .1, .4), vec3(.3, .6, .8), 1.0 - eye_dir.y);
 	sky += sun_color * min(pow(sun_amount, 1500.0) * 5.0, 1.0);
@@ -80,57 +66,148 @@ vec3 render_sky_color(
 	return abs(sky);
 }
 
+// ----------------------------------------------------------------------------
+// Density
+// ----------------------------------------------------------------------------
+#include "../lib/ashima-noise/src/common.glsl"
+#include "../lib/ashima-noise/src/classicnoise3d.glsl"
+#include "../lib/ashima-noise/src/noise3d.glsl"
+#include "../lib/ashima-noise/src/cellular3d.glsl"
+
+#include "fbm.h"
+DECL_FBM_FUNC(fbm_simple_but_nice, 5, abs(snoise(p)))
+DECL_FBM_FUNC(fbm_low_freq_perlin, 4, cnoise(p))
+DECL_FBM_FUNC(fbm_low_freq_worley, 3, cellular(p).r)
+
+//#define TEX
+#ifdef TEX
+Texture3D u_tex_noise : register(t1);
+SamplerState u_sampler0 : register(s0);
+#endif
+
 float density_func(
-	_in(vec3) pos,
-	_in(float) h
+	_in(vec3) pos_in,
+	_in(float) height
 ){
-	vec3 p = pos * cld_noise_factor + cld_wind_dir;
-	float base = fbm_final(p);
+	vec3 pos = pos_in * cld_noise_factor + wind_dir;
 
-// my old method
-//	return base * smoothstep (cld_coverage, cld_coverage + .035, base);
+	float base = 0.;
+	{
+#ifdef TEX
+		base = u_tex_noise.SampleLevel(u_sampler0, pos, 0).r;
+#endif
 
-// book equiv method
-//	return smoothstep(cld_coverage, 1., base);
+		base =  fbm_simple_but_nice(pos * 2.03, 2.64, .5, .5);
 
-// GPU Pro 7
-	float base_with_coverage = remap(base, cld_coverage, 1., 0., 1.);
-	return clamp(base_with_coverage * cld_coverage, 0, 1);
+		//float p = fbm_low_freq_perlin(pos * 4., 2., .5, .5);
+		//float w = 1. - fbm_low_freq_worley(pos * 4., 4., .5, .5);
+		//base = remap(p, -w, 1., 0., 1.);
+	}
+
+	{
+#define cld_coverage (.35)
+		// my old method
+			//return base * smoothstep (cld_coverage, cld_coverage + .035, base);
+		// book equiv method
+			//return smoothstep(cld_coverage, 1., base);
+		// GPU Pro 7
+			float base_with_coverage = remap(base, cld_coverage, 1., 0., 1.);
+			return clamp(base_with_coverage * cld_coverage, 0, 1);
+	}
 }
 
-float illuminate_volume(
-	_inout(volume_sampler_t) cloud,
+// ----------------------------------------------------------------------------
+// Volumetrics
+// ----------------------------------------------------------------------------
+#define cld_march_steps		(50)
+#define cld_thick			(100.)
+#define illum_march_steps	(6)
+
+#define sigma_absobtion		(0.)
+#define sigma_scattering	(1.)
+
+struct volumetric_sampler_t {
+	vec3 origin; // start of ray
+	vec3 pos; // current pos of acccumulation ray
+	float height;
+	float transmittance;
+	vec3 radiance;
+	float alpha;
+};
+
+volumetric_sampler_t define_volume(
+	_in(vec3) origin
+){
+	volumetric_sampler_t v = _begin(volumetric_sampler_t)
+		origin,
+		origin,
+		0.,
+		1.,
+		vec3(0, 0, 0),
+		0.
+	_end;
+	return v;
+}
+
+float illuminate_volumetric(
+	_in(vec3) origin,
+	_in(float) height,
 	_in(vec3) V,
 	_in(vec3) L
 ){
-	return 1.; // exp(cloud.height) / 2.02;
+#if 1
+	return exp(height) / 2.02;
+#else
+
+	const float dt = cld_thick / float(illum_march_steps);
+	volumetric_sampler_t vol = define_volume(origin);
+	vol.pos += L * (dt / 2.); // don't sample just where the main raymarcher is
+
+#ifdef HLSL
+	[unroll(illum_march_steps)]
+#endif
+	for (int i = 0; i < illum_march_steps; i++) {
+		float density = density_func(vol.pos, 0.);
+
+		vol.transmittance *= exp(- density * sigma_scattering * dt);
+
+		vol.pos += L * dt;
+	}
+
+	return vol.transmittance;
+#endif
 }
 
-void intersect_sphere(
-	_in(ray_t) ray,
-	_in(sphere_t) sphere,
-	_inout(hit_t) hit
-) {
-	vec3 rc = sphere.origin - ray.origin;
-	float radius2 = sphere.radius * sphere.radius;
-	float tca = dot(rc, ray.direction);
-	float d2 = dot(rc, rc) - tca * tca;
-	float thc = sqrt(radius2 - d2);
-	float t0 = tca - thc;
-	float t1 = tca + thc;
+void integrate_volumetric(
+	_inout(volumetric_sampler_t) vol,
+	_in(vec3) V,
+	_in(vec3) L,
+	_in(float) density,
+	_in(float) dt
+){
+	// integrate output radiance (here essentially color)
+	vol.radiance += 
+		(density * sigma_scattering) * 
+		illuminate_volumetric(vol.pos, vol.height, V, L) *
+		vol.transmittance *
+		dt;
 
-	vec3 impact = ray.origin + ray.direction * t0;
-	hit.t = t0;
-	hit.material_id = sphere.material;
-	hit.origin = impact;
-	hit.normal = (impact - sphere.origin) / sphere.radius;
+	// change in transmittance (follows Beer-Lambert law)
+	float T_i = exp(- density * sigma_scattering * dt);
+	// Update accumulated transmittance
+	vol.transmittance *= T_i;
+
+	// accumulate opacity
+	vol.alpha += (1. - T_i) * (1. - vol.alpha);
 }
 
+// ----------------------------------------------------------------------------
+// Raymarching
+// ----------------------------------------------------------------------------
 vec4 render_clouds(
 	_in(ray_t) eye
 ){
-	const int steps = cld_march_steps;
-	const float march_step = cld_thick / float(steps);
+	const float march_step = cld_thick / float(cld_march_steps);
 
 #ifdef SPHERE
 	hit_t hit = no_hit;
@@ -144,19 +221,22 @@ vec4 render_clouds(
 #endif
 
 	vec3 iter = projection * march_step;
-	volume_sampler_t cloud = begin_volume(origin, cld_absorb_coeff);
+	volumetric_sampler_t cloud = define_volume(origin);
 
 #ifdef HLSL
 	[fastopt] [loop]
 #endif
-	for (int i = 0; i < steps; i++) {
-		cloud.height = float(i) / float(steps);
-		float dens = density_func(cloud.pos, cloud.height);
+	for (int i = 0; i < cld_march_steps; i++) {
+		cloud.height = float(i) / float(cld_march_steps);
+		
+		float density = density_func(cloud.pos, cloud.height);
 
-		integrate_volume(
+		integrate_volumetric(
 			cloud,
-			eye.direction, cld_sun_dir,
-			dens, march_step);
+			eye.direction,
+			sun_dir,
+			density,
+			march_step);
 
 		cloud.pos += iter;
 
@@ -164,7 +244,7 @@ vec4 render_clouds(
 	}
 
 	float cutoff = dot(eye.direction, vec3(0, 1, 0));
-	return vec4(cloud.C, cloud.alpha * smoothstep(.0, .2, cutoff));
+	return vec4(cloud.radiance, cloud.alpha * smoothstep(.0, .2, cutoff));
 }
 
 vec3 render(
