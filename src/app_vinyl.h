d932d6af3369021730d05929fbf9e9c1376c9c70
valentingalea <valentingalea@gmail.com>
2016-09-18 01:41:48 +0100

    primitive tonearm; shadows;

--- src/app_vinyl.h
+++ src/app_vinyl.h
@@ -16,7 +16,7 @@ DECL_FBM_FUNC(fbm, 4, noise_func)
 
 vec3 background(_in(ray_t) ray)
 {
-	return vec3(.6, .6, .6);
+	return vec3(1, 1, 1);
 }
 
 #define mat_groove 1
@@ -58,11 +58,11 @@ void setup_scene()
 void setup_camera(_inout(vec3) eye, _inout(vec3) look_at)
 {
 #if 1
-	eye = vec3(0, 6, 7);
-	look_at = vec3(0, -3, 0);
+	eye = vec3(0, 5.75, 6.75);
+	look_at = vec3(0, -2.5, 0);
 #else
-	eye = vec3(4, 3, 3);
-	look_at = vec3(4, 0, 0);
+	eye = vec3(0, 0, 15);
+	look_at = vec3(0, 0, 0);
 #endif
 }
 
@@ -101,12 +101,12 @@ vec2 sdf_platter(_in(vec3) pos)
 	vec2 label = vec2(sd_y_cylinder(p, 2., thick),
 		mat_label);
 	vec2 logo = vec2(
-		sdf_logo(p, thick - .0075),
+		sdf_logo(p, thick - .0175),
 		mat_logo);
 	//float center_hole =
 	//	sd_y_cylinder(p, .25, thick * 4.);
-	float spc = sd_y_cylinder(p, .15, .8);
-	float sps = sd_sphere(p - vec3(0, .4, 0), .15);
+	float spc = sd_y_cylinder(p, .10, .6);
+	float sps = sd_sphere(p - vec3(0, .3, 0), .10);
 	vec2 spindle = vec2(
 		op_add(spc, sps), mat_shiny);
 	
@@ -125,9 +125,55 @@ vec2 sdf_platter(_in(vec3) pos)
 	return vec2(op_sub(d4.x, defect), d4.y);
 }
 
-vec2 sdf(_in(vec3) pos)
+// Distance to line segment between <a> and <b>, used for fCapsule() version 2below
+float fLineSegment(vec3 p, vec3 a, vec3 b) {
+	vec3 ab = b - a;
+	float t = clamp(dot(p - a, ab) / dot(ab, ab), 0., 1.);
+	return length((ab*t + a) - p);
+}
+
+// Capsule version 2: between two end points <a> and <b> with radius r 
+float sd_capsule(vec3 p, vec3 a, vec3 b, float r) {
+	return fLineSegment(p, a, b) - r;
+}
+
+vec2 sdf(_in(vec3) p)
 {
-	return sdf_platter(pos);
+	vec2 plat = sdf_platter(p);
+
+	vec3 base_p = vec3(-7, 0, -5);
+	float platter = sd_y_cylinder(p, 6.25, 1.);
+	float base_0 = sd_y_cylinder(p - base_p, 3., .25);
+	float base_1 = op_sub(base_0, platter);
+	float base_2 = sd_y_cylinder(p - base_p, 1.15, 1.05);
+	float base_12 = op_add(base_1, base_2);
+	vec2 base_a = vec2(base_12, mat_shiny);
+	vec2 base_b = vec2(sd_y_cylinder(p - base_p, 0.5, 2.5), mat_shiny);
+	vec2 base = op_add(base_a, base_b);
+
+	const float D = .15;
+	const float H = 1.;
+	vec3 a1 = vec3(-6, H, -3);
+	vec3 a11 = vec3(-4.25, H, 2);
+	vec3 a2 = vec3(-4.1, H, 2.45);
+	vec3 a33 = vec3(-3.5, H, 3);
+	vec3 a3 = vec3(-2, H, 4);
+	float arm1 = sd_capsule(p, base_p + vec3(-1, H, -2), a1, D);
+	float arm2 = sd_capsule(p, a1, a11, D);
+	float arm3 = sd_capsule(p, a33, a3, D);
+	vec2 armb = sd_bezier(a11, a2, a33, p, D);
+	vec2 arm = vec2(
+		op_add(op_add(op_add(arm1, arm2), arm3), armb.x),
+		mat_shiny);
+
+	vec3 pp = mul(p - a3, mul(mul(rotate_around_y(-35.), rotate_around_z(-45.)), rotate_around_x(-20.)));
+	vec2 cartrige = vec2(
+		sd_box(pp - vec3(.45, 0, 0), vec3(.5, .15, .15)),
+		mat_shiny);
+
+	vec2 tonearm = op_add(op_add(base, arm), cartrige);
+
+	return op_add(plat, tonearm);
 }
 
 vec3 sdf_normal(_in(vec3) p)
@@ -153,6 +199,9 @@ float pulse(_in(float) x)
 	return saw(x + .5) - saw(x);
 }
 
+_constant(vec3) sun_dir = //normalize(vec3(2, 4, -3));
+	normalize(vec3(-1, 4, -3));
+
 vec3 illuminate(
 	_in(vec3) eye,
 	_inout(hit_t) hit
@@ -161,7 +210,7 @@ vec3 illuminate(
 	return vec3(hit.normal);
 #endif
 
-	vec3 L = normalize(vec3(2, 4, -3));
+	vec3 L = sun_dir;
 	vec3 V = normalize(eye - hit.origin); // view direction
 
 	material_t mat = get_material(hit.material_id);
@@ -179,7 +228,7 @@ vec3 illuminate(
 			float rr = r + .07575 *
 				noise_iq(hit.origin * 2.456);
 				//fbm(hit.origin * 4.07, 2.08, .5, .5);
-			float s = pulse(rr * 20.);
+			float s = pulse(rr * 24.);
 			if (s > 0.) {
 				N = normalize(N + B);
 				N = reflect(N, vec3(0, 1, 0));
@@ -187,7 +236,7 @@ vec3 illuminate(
 		}
 		if (hit.material_id == mat_dead_wax) {
 			float s = saw(r * 4.);
-			N = normalize(N + B * (s > .9));
+			N = normalize(N + B * float(s > .9));
 		}
 		//return N;
 		vec3 T = cross(B, N);
@@ -222,12 +271,12 @@ vec3 illuminate(
 		return diffuse + specular;
 	} else {
 		hit.normal = sdf_normal(hit.origin);
-#if 1
+#if 0
 		if (hit.material_id == mat_label || hit.material_id == mat_logo) {
 			float r = length(hit.origin);
 			vec3 B = hit.origin / r;
 			float s = saw(r * .9);
-			hit.normal = normalize(hit.normal + B * (s > .975));
+			hit.normal = normalize(hit.normal + B * float(s > .975));
 		}
 #endif
 
@@ -239,12 +288,38 @@ vec3 illuminate(
 	}
 }
 
+float sdf_shadow(_in(ray_t) ray)
+{
+	const int steps = 20;
+	const float end = 5.;
+	const float penumbra_factor = 16.;
+	const float darkest = .05;
+	float t = 0.;
+	float umbra = 1.;
+
+	for (int i = 0; i < steps; i++) {
+		vec3 p = ray.origin + ray.direction * t;
+		vec2 d = sdf(p);
+
+		if (t > end) break;
+		if (d.x < .005) {
+			return darkest;
+		}
+
+		t += d.x;
+		// from http://iquilezles.org/www/articles/rmshadows/rmshadows.htm
+		umbra = min(umbra, penumbra_factor * d.x / t);
+	}
+
+	return umbra;
+}
+
 vec3 render(
 	_in(ray_t) ray,
 	_in(vec3) point_cam
 ){
 	const int steps = 80;
-	const float end = 20.;
+	const float end = 40.;
 
 	float rot = u_time * 200.;
 	if (u_mouse.z > 0.) {
@@ -267,8 +342,15 @@ vec3 render(
 				vec3(0, 1, 0), // normal
 				p // point of impact				
 			_end;    
-			
-			return illuminate(ray.origin, h);
+		
+			float sh = 1.;
+#if 1
+			ray_t sh_ray = _begin(ray_t)
+				p + sun_dir * 0.05, sun_dir
+				_end;
+			sh = sdf_shadow(sh_ray);
+#endif   
+			return illuminate(ray.origin, h) * sh;
 		}
 
 		t += d.x;
