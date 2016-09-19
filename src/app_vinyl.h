9c23482d8ff09c946832e117191e7f5b310bea47
valentingalea <valentingalea@gmail.com>
2016-09-19 23:52:08 +0100

    managed to do a nice collar and finger grip

--- src/app_vinyl.h
+++ src/app_vinyl.h
@@ -137,10 +137,9 @@ float sd_capsule(vec3 p, vec3 a, vec3 b, float r) {
 
 vec2 sdf(_in(vec3) p)
 {
-	vec2 plat = sdf_platter(p);
-
 	vec3 base_p = vec3(-7, 0, -5);
-/*
+#define JUST_TONE_ARM
+#ifndef JUST_TONE_ARM
 	float platter = sd_y_cylinder(p, 6.25, 1.);
 	float base_0 = sd_y_cylinder(p - base_p, 3., .25);
 	float base_1 = op_sub(base_0, platter);
@@ -149,7 +148,7 @@ vec2 sdf(_in(vec3) p)
 	vec2 base_a = vec2(base_12, mat_shiny);
 	vec2 base_b = vec2(sd_y_cylinder(p - base_p, 0.5, 2.5), mat_shiny);
 	vec2 base = op_add(base_a, base_b);
-*/
+#endif
 	const float D = .15;
 	const float H = 1.;
 	vec3 a1 = vec3(-6, H, -3);
@@ -165,50 +164,66 @@ vec2 sdf(_in(vec3) p)
 		op_add(op_add(op_add(arm1, arm2), arm3), armb.x),
 		mat_shiny);
 
-/*
-	vec3 ang = vec3(20, 35, 45);
-#ifdef HLSL
-	ang *= -1.;
-#endif
-	vec3 pp = mul(p - a3,
-		mul(mul(rotate_around_y(ang.y),
-		rotate_around_z(ang.z)),
-		rotate_around_x(ang.x)));
-	vec2 cartrige = vec2(
-		sd_box(pp - vec3(.45, 0, 0), vec3(.5, .15, .15)),
-		mat_shiny);
-*/
-
+	// construct a rotation matrix
+	// from the orientation of the arm
 	vec3 arm_fwd = normalize(a3 - a33);
 	vec3 arm_up = vec3(0, 1, 0);
 	vec3 arm_right = cross(arm_fwd, arm_up);
-	mat3 arm_xform = mat3(arm_fwd, arm_up,
-	arm_right);
-	
+	mat3 arm_xform = mat3(
+		arm_fwd,
+		arm_up,
+		arm_right);
+
+	// collar 'clr' (or flange)
 	vec3 clr_p = p - a3;
 	float clr_r = D * 1.5;
 	float collar = sd_cylinder(clr_p,
-		vec3(0), vec3(0) + arm_fwd * .05,
+		vec3(0, 0, 0), vec3(0, 0, 0) + arm_fwd * .05,
 		clr_r);
-		
+
+	// finger lift (or grip) 'fl'
 	const float fl_w = .045;
 	const float fl_h = .020;
-	const float fl_len1 = clr_r * 2.;
-	vec3 pp = mul(clr_p - vec3(0, 0, fl_len1),
-		 mul(rotate_around_x(30), arm_xform));
+	float fl_len1 = clr_r * 1.;
+	float fl_len2 = fl_len1 * 1.2;
+
+	// the first fl part is rotated
+	// and 'pushed' back to fit inside the collar
+	// a new (local) transform space is created by
+	// combining the arm one plus a new rotation
+	mat3 fl_rot =
+		mul(arm_xform, rotate_around_x(45.));
+	vec3 pp = mul(clr_p -
+		arm_right * clr_r -
+		arm_up * clr_r,
+		fl_rot);
 	float fl1 = sd_box(pp,
 		vec3(fl_w, fl_h, fl_len1));
-	float finger_lift = fl1;
-		
+
+	// the second fl part is positioned
+	// relative to the first, working in this
+	// new local transform space
+	mat3 fl_rot2 = rotate_around_x(-45.);
+	float fl2 = sd_box(
+		mul(pp - vec3(0, 0, fl_len1), fl_rot2) - vec3(0, 0, fl_len2),
+		vec3(fl_w, fl_h, fl_len2));
+	float finger_lift = op_add(fl1, fl2);
+
 	vec2 cartrige = vec2(
-		op_add(collar, finger_lift),
+		op_add(collar,
+			finger_lift),
 		mat_shiny);
 
-	vec2 tonearm = op_add(
-	arm, //op_add(base, arm),
-	cartrige);
+#ifdef JUST_TONE_ARM
+	return op_add(arm, cartrige);
+#else
+	vec2 plat = sdf_platter(p);
+	vec2 tonearm =
+		op_add(op_add(base, arm),
+			cartrige);
 
-	return tonearm;//op_add(plat, tonearm);
+	return op_add(plat, tonearm);
+#endif
 }
 
 vec3 sdf_normal(_in(vec3) p)
